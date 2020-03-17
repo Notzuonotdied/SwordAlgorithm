f6ca828aacc0fef388b99e19a68707c13ad4d2b8
Notzuonotdied <Notzuonotdied@qq.com>
2020-03-17 20:43:11 +0800

    Add: 把字符串转换成整数。

---
+++ src/JavaOffer/Demo49.java
@@ -0,0 +1,14 @@
+package JavaOffer;
+
+public class Demo49 {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Solution1 {
+        public static int StrToInt(String str) {
+            // TODO: 把字符串转换成整数
+            return 0;
+        }
+    }
+}
