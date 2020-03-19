ff27cc807e378099d91b320fc916ea0e3eff00cf
Notzuonotdied <Notzuonotdied@qq.com>
2020-03-19 21:04:27 +0800

    Add: 表示数值的字符串。

---
+++ src/JavaOffer/Demo53.java
@@ -0,0 +1,14 @@
+package JavaOffer;
+
+public class Demo53 {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Solution1 {
+        public boolean isNumeric(char[] str) {
+            // TODO 表示数值的字符串
+            return false;
+        }
+    }
+}
