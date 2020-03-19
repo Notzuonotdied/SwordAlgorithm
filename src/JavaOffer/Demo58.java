5dcdc521e9a2de16bf4a5655ed13b43fbc53f5f3
Notzuonotdied <Notzuonotdied@qq.com>
2020-03-19 23:23:59 +0800

    Add: 对称的二叉树。

---
+++ src/JavaOffer/Demo58.java
@@ -0,0 +1,26 @@
+package JavaOffer;
+
+public class Demo58 {
+    public static void main(String[] args) {
+
+    }
+
+    public class Solution {
+        boolean isSymmetrical(TreeNode pRoot) {
+            // TODO 对称的二叉树
+            return false;
+        }
+    }
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+}
