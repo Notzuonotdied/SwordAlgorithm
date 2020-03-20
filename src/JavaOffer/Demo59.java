f42d5dfc6420e59ff13ad20ab7fe255b37d78b08
Notzuonotdied <Notzuonotdied@qq.com>
2020-03-20 21:10:01 +0800

    update: 占坑「按之字形顺序打印二叉树」。

--- src/JavaOffer/Demo59.java
+++ src/JavaOffer/Demo59.java
@@ -7,21 +7,10 @@ public class Demo59 {
 
     }
 
-    public static class Solution {
-        ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-            depth(pRoot, 1, list);
-            return list;
-        }
-
-        private void depth(TreeNode root, int depth, ArrayList<ArrayList<Integer>> list) {
-            if (root == null) return;
-            if (depth > list.size())
-                list.add(new ArrayList<>());
-            list.get(depth - 1).add(root.val);
-
-            depth(root.left, depth + 1, list);
-            depth(root.right, depth + 1, list);
+    public static class Solution1 {
+        public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+            // TODO 按之字形顺序打印二叉树
+            return null;
         }
     }
 
