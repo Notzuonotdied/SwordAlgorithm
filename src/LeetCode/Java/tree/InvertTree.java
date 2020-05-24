5383d6dbe275db177cbdd3660a1af87fb9ef0344
Notzuonotdied <Notzuonotdied@qq.com>
2020-05-24 20:49:16 +0800

    add: 使用递归翻转二叉树。

---
+++ src/LeetCode/Java/tree/InvertTree.java
@@ -0,0 +1,54 @@
+package LeetCode.Java.tree;
+
+public class InvertTree {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Solution1 {
+        /**
+         * 翻转一棵二叉树。
+         *
+         * 示例：
+         * 输入：
+         *      4
+         *    /   \
+         *   2     7
+         *  / \   / \
+         * 1   3 6   9
+         * 输出：
+         *      4
+         *    /   \
+         *   7     2
+         *  / \   / \
+         * 9   6 3   1
+         *
+         * 来源：力扣（LeetCode）
+         * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+         * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+         *
+         * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00% 的用户
+         * 内存消耗 :37.1 MB, 在所有 Java 提交中击败了6.00%的用户
+         */
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
+            TreeNode temp = root.left;
+            root.left = invertTree(root.right);
+            root.right = invertTree(temp);
+            return root;
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
--- src/LeetCode/Java/tree/Readme.md
+++ src/LeetCode/Java/tree/Readme.md
@@ -14,6 +14,7 @@
 | 代码 | 解法 | 链接 |
 | ---- | ---- | ---- |
 | [合并二叉树](MergeTrees.java) | 递归 | [给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。](https://leetcode-cn.com/problems/merge-two-binary-trees/) |
+| [翻转二叉树](InvertTree.java) |  递归 | [翻转一棵二叉树。](https://leetcode-cn.com/problems/invert-binary-tree/) |
 
 # 附录
 
