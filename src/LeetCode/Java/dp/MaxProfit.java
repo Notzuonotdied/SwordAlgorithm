53cee337345898d689f7359bbc50043749b4b2d5
Notzuonotdied <Notzuonotdied@qq.com>
2020-06-06 22:01:42 +0800

    update: 买卖股票最佳时机题目重做。

--- src/LeetCode/Java/dp/MaxProfit.java
+++ src/LeetCode/Java/dp/MaxProfit.java
@@ -3,6 +3,7 @@ package LeetCode.Java.dp;
 public class MaxProfit {
     public static void main(String[] args) {
         System.out.println(Solution1.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(Solution2.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
     private static class Solution1 {
@@ -35,4 +36,35 @@ public class MaxProfit {
             return profile;
         }
     }
+
+
+    public static class Solution2 {
+        /**
+         * 时间：2020年06月06日22:00:56
+         * <p>
+         * 假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？
+         * <p>
+         * 这个问题其实包含了一个重复子问题，就是每次都需要找局部最优解。
+         * <p>
+         * 执行用时 :2 ms, 在所有 Java 提交中击败了66.01% 的用户
+         * 内存消耗 :39.8 MB, 在所有 Java 提交中击败了100.00%的用户
+         */
+        public static int maxProfit(int[] prices) {
+            if (prices.length == 0) {
+                return 0;
+            }
+
+            int[] dp = new int[1];
+            int minProfile = prices[0];
+            for (int i = 1; i < prices.length; ++i) {
+                dp[0] = Math.max(dp[0], prices[i] - minProfile);
+                // 利润最低点
+                minProfile = Math.min(minProfile, prices[i]);
+            }
+
+            return dp[0];
+        }
+    }
+
+
 }
