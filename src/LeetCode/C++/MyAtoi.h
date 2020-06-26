91a65b6cf1b83f020b6dd2cce3da1402c3f7c2b2
oliewalcp <811299838@qq.com>
2020-06-26 11:09:11 +0800

    字符串转整数

---
+++ src/LeetCode/C++/MyAtoi.h
@@ -0,0 +1,74 @@
+#ifndef MY_ATOI_H
+#define MY_ATOI_H
+
+/* 字符串转int数字
+ * @str  输入的字符串
+ * @return  如果小于最小值，则返回最小值；如果大于最大值，则返回最大值；如果非法，则返回0
+ */
+static int MyAtoi(const std::string &str)
+{
+    long long result = 0;
+    bool conver_start = false; // 标记是否已开始转换
+    bool is_negative = false;
+
+    for(const char ch : str)
+    {
+        if(ch == ' ')
+        {
+            if(conver_start)
+            {
+                break;
+            }
+        }
+        else if((ch > '9' || ch < '0') && ch != '-' && ch != '+')
+        {
+            if(conver_start)
+            {
+                break;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+        else 
+        {
+            if(ch == '-' || ch == '+')
+            {
+                if(conver_start)
+                {
+                    break;
+                }
+                is_negative = ch == '-';
+            }
+            else 
+            {
+                result = result * 10 - '0' + ch;
+                if(is_negative && (-result) < static_cast<long long>(INT_MIN))
+                {
+                    return INT_MIN;
+                }
+                else if(!is_negative && result > INT_MAX)
+                {
+                    return INT_MAX;
+                }
+            }
+            conver_start = true;
+        }
+    }
+    if(is_negative)
+    {
+        result = -result;
+    }
+    if(result <= static_cast<long long>(int(INT_MIN)))
+    {
+        return INT_MIN;
+    }
+    if(result > INT_MAX)
+    {
+        return INT_MAX;
+    }
+    return result;
+}
+
+#endif // MY_ATOI_H
