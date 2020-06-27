8dc7654bc6d8aa9b818184779242ff34698d7980
oliewalcp <811299838@qq.com>
2020-06-27 23:42:48 +0800

    Update: 整数按位反转泛化

--- src/LeetCode/C++/IntergerReverse.h
+++ src/LeetCode/C++/IntergerReverse.h
@@ -1,11 +1,29 @@
 #ifndef INTERGER_REVERSE_H
 #define INTERGER_REVERSE_H
 
+/* 整数按数位反转
+ * @v  输入数
+ * @return  结果，不检查溢出
+ */
+template<typename _Tp>
+_Tp reverse(const _Tp &v)
+{
+    _Tp input = v;
+    _Tp result = 0;
+    while(input != 0)
+    {
+        result = result * 10 + input % 10;
+        input /= 10;
+    }
+    return result;
+}
+
 /* 整数按位反转
  * @x  输入数
- * @return  如果越界，则返回0
+ * @return  如果溢出，则返回0
  */
-static int reverse(int x)
+template<>
+int reverse<int>(const int &x)
 {
     long long input = x;
     long long result = 0;
