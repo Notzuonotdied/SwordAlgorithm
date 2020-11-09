2576df6f904d46821297d02bd71852eeaa2e5ea1
Notzuonotdied <Notzuonotdied@qq.com>
2020-11-09 22:01:28 +0800

    add: 使用回溯思想解决「括号生成」的问题。

---
+++ src/LeetCode/Java/backtrack/GenerateParenthesis.java
@@ -0,0 +1,56 @@
+package LeetCode.Java.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+    public static void main(String[] args) {
+
+    }
+
+    private static class Solution1 {
+        /**
+         * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+         *
+         * 示例：
+         *
+         * 输入：n = 3
+         * 输出：[
+         *        "((()))",
+         *        "(()())",
+         *        "(())()",
+         *        "()(())",
+         *        "()()()"
+         *      ]
+         *
+         * 来源：力扣（LeetCode）
+         * 链接：https://leetcode-cn.com/problems/generate-parentheses
+         * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+         */
+        public List<String> generateParenthesis(int n) {
+            List<String> ans = new ArrayList<>();
+            StringBuilder cur = new StringBuilder();
+            backtrace(ans, cur, 0, 0, n);
+            return ans;
+        }
+
+        private void backtrace(List<String> ans, StringBuilder cur, int open, int close, int max) {
+            // 因为左右括号，所有需要两倍
+            if (cur.length() == max * 2) {
+                ans.add(cur.toString());
+                return;
+            }
+            if (open < max) {
+                cur.append('(');
+                backtrace(ans, cur, open + 1, close, max);
+                cur.deleteCharAt(cur.length() - 1);
+            }
+            // 确保右括号的数量要等于左括号的数量
+            if (close < open) {
+                cur.append(')');
+                backtrace(ans, cur, open, close + 1, max);
+                cur.deleteCharAt(cur.length() - 1);
+            }
+        }
+    }
+}
