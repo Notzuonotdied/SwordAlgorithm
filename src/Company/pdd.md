5cc0dab92498fc6ca5ea1b1c03f3c98c7a9f8c8e
Notzuonotdied <Notzuonotdied@qq.com>
2021-12-19 21:18:00 +0800

    重刷「二叉树的层序遍历」题目。

--- src/Company/pdd.md
+++ src/Company/pdd.md
@@ -3,6 +3,7 @@
 | 代码 | 解法 | 链接 |
 | ---- | ---- | ---- |
 | [206.反转链表](../LeetCode/Java/linklist/ReverseList.java) | | [反转一个单链表。](https://leetcode-cn.com/problems/reverse-linked-list/) |
+| [102. 二叉树的层序遍历](../LeetCode/Java/tree/LevelOrder.java) |  BFS | [给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。](https://leetcode-cn.com/problems/binary-tree-level-order-traversal/) |
 
 # 附录
 
--- src/LeetCode/Java/tree/LevelOrder.java
+++ src/LeetCode/Java/tree/LevelOrder.java
@@ -2,74 +2,80 @@ package LeetCode.Java.tree;
 
 import common.java.TreeNode;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
-/**
- * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
- *
- *
- *
- * 示例：
- * 二叉树：[3,9,20,null,null,15,7],
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
- *
- * 返回其层次遍历结果：
- *
- * [
- *   [3],
- *   [9,20],
- *   [15,7]
- * ]
- *
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- */
 public class LevelOrder {
     public static void main(String[] args) {
 
     }
 
-    private static class Solution1 {
+    private interface ISolution {
+        List<List<Integer>> levelOrder(TreeNode root);
+    }
+
+    private static class Solution1 implements ISolution {
         /**
-         * 更多详见：BFS 的使用场景总结：层序遍历、最短路径问题
+         * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+         * 示例：
+         * 二叉树：[3,9,20,null,null,15,7],
+         *     3
+         *    / \
+         *   9  20
+         *     /  \
+         *    15   7
+         * 返回其层序遍历结果：
+         * [
+         *   [3],
+         *   [9,20],
+         *   [15,7]
+         * ]
          *
+         * 来源：力扣（LeetCode）
+         * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+         * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+         *
+         * 更多详见：BFS 的使用场景总结：层序遍历、最短路径问题
+         * <p>
          * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
+         *
+         * 执行用时：1 ms, 在所有 Java 提交中击败了90.02% 的用户
+         * 内存消耗：38.6 MB, 在所有 Java 提交中击败了54.39% 的用户
          */
         public List<List<Integer>> levelOrder(TreeNode root) {
-            List<List<Integer>> res = new ArrayList<>();
+            List<List<Integer>> ans = new ArrayList<>();
             if (root == null) {
-                return res;
+                return ans;
             }
-            Queue<TreeNode> stack = new ArrayDeque<>();
-            stack.add(root);
+            Queue<TreeNode> queue = new ArrayDeque<>();
+            queue.add(root);
 
-            TreeNode temp;
-            while (!stack.isEmpty()) {
-                int n = stack.size();
-                List<Integer> levelRes = new ArrayList<>();
-                for (int i = 0; i < n; ++i) {
-                    temp = stack.poll();
-                    if (temp.left != null) {
-                        stack.add(temp.left);
+            TreeNode tmp;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> levelRes = new ArrayList<>(size);
+                // 逐层遍历
+                for (int i = 0; i < size; ++i) {
+                    // 每次都弹出队列头
+                    tmp = queue.poll();
+                    if (tmp == null) {
+                        continue;
                     }
-
-                    if (temp.right != null) {
-                        stack.add(temp.right);
+                    // 逐次将树的左右节点追加进去
+                    if (tmp.left != null) {
+                        queue.add(tmp.left);
                     }
-
-                    levelRes.add(temp.val);
+                    if (tmp.right != null) {
+                        queue.add(tmp.right);
+                    }
+                    // 保存当前层的结果
+                    levelRes.add(tmp.val);
                 }
-                res.add(levelRes);
+                ans.add(levelRes);
             }
-
-            return res;
+            return ans;
         }
     }
 }
